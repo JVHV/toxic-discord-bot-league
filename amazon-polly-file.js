d60e5d9b479c2b62ba42f1cec08e40687488fb77
Juan Vicente Hernández <juanvhernandez.12@gmail.com>
2020-07-19 14:58:27 -0400

    Added some internal jokes for my friends

--- amazon-polly-file.js
+++ amazon-polly-file.js
@@ -19,25 +19,25 @@ function Roast(newNum, VictimName, FirstBlood) {
     console.log(randomNum);
     switch (randomNum) {
         case 1:
-            roast = VictimName + ", I suggest you go to Practice Mode to learn how to play. PLEASE! You suck balls my guy!";
+            roast = "Daniel dice, como un juego puede estar tan mal hecho";
             return roast;
         case 2:
-            roast = VictimName + ", go back to playing C S GO. Wait. Are you even good at C S GO?"
+            roast = VictimName + ", go back to playing Valorant. Wait. Are you even good at Valorant?"
             return roast;
         case 3:
             roast = VictimName + ", Can you like do something with your life other than League? You are legit trash"
             return roast;
         case 4:
-            roast = "Oh my God. I cannot watch this game anymore. All of you are terrible."
+            roast = "MALDITO JUEGO MAL HECHO"
             return roast;
         case 5:
-            roast = VictimName + " Just stop playing. Please."
+            roast = VictimName + " DEJA DE JUGAR POR FAVOR."
             return roast;
         case 6:
             roast = "Holy shit " + VictimName + ". Can you land anything? Next game, kick " + VictimName;
             return roast;
         case 7:
-            roast = "God damn you are so fucking trash " + VictimName;
+            roast = "ERES UNA BASURA " + VictimName;
             return roast;
         case 8:
             roast = "Wow " + VictimName + ". You know it's a shame you play so much League but you are still trash."
@@ -52,7 +52,7 @@ function Roast(newNum, VictimName, FirstBlood) {
             roast = "You're a fucking piece of shit " + VictimName + ". You know that?"
             return roast;
         case 12:
-            roast = "Literally I can win in a one v one against you" + VictimName + ". You are shit."
+            roast = "go fuck a cat"
             return roast;
         case 13:
             roast = "YOU. ARE. SHIT " + VictimName;
@@ -64,13 +64,13 @@ function Roast(newNum, VictimName, FirstBlood) {
             roast = VictimName + "Just stop. PLEASEEEEEE."
             return roast;
         case 16:
-            roast = "Get shit on " + VictimName;
+            roast = "MALDITO NINO GORDO";
             return roast;
         case 17:
             roast = "God damn. you suck " + VictimName;
             return roast;
         case 18:
-            roast = "Get your kills up for crying out loud " + VictimName;
+            roast = "how can an otaku play chinese games this bad";
             return roast;
         case 19:
             roast = "You are so... fucking... shit " + VictimName;
@@ -85,13 +85,13 @@ function Roast(newNum, VictimName, FirstBlood) {
             roast = "Get your kills up for crying out loud " + VictimName;
             return roast;
         case 23:
-            roast = "You have a peanut brain " + VictimName;
+            roast = "VIVAN LOS GATOS";
             return roast;
         case 23:
-            roast = "You have a peanut brain " + VictimName;
+            roast = "ERES PEOR QUE EL HECTOR"
             return roast;
         case 21:
-            roast = "First Blood? Really? Wow you really suck. You fucking lost."
+            roast = "First Blood? Really? Wow you really suck. You fucking loser."
             return roast;
 
     }
